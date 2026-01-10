0da8d9cf013b28cece5ff40264afcd80569a7727
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2026-01-10 03:01:10 -0500

    django secrets

--- thedotschool/settings.py
+++ thedotschool/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ff#mf4enn@k542n$sg#javy6b_@dtjujs)$9hp9+7#qy13oz5r'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
